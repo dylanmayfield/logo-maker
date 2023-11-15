078c7cb00f342408497a7b80ae3115cda9bbb6b9
Dylan Mayfield <dylanmayfield18@gmail.com>
2023-11-15 15:39:06 -0600

    updated index.js and shapes.js, worked out bugs

--- index.js
+++ index.js
@@ -4,10 +4,10 @@
 
 const inquirer = require("inquirer");
 const fs = require('fs');
-const {Triangle, Square, Circle} = require('./lib/shapes');
+const { Triangle, Square, Circle } = require('./lib/shapes');
 
 function writeToFile(fileName, svgContent) {
-        fs.writeFile(fileName, svgContent, (err) => {
+    fs.writeFile(fileName, svgContent, (err) => {
         if (err) throw err;
         console.log('The file has been saved!');
     });
@@ -24,57 +24,58 @@ function renderLogo(answers) {
     }
 
 
-const svgContent = `
-<svg width="300" height="200">
-    <rect width="100%" height="100%" fill="${answers.shapeColor}" />
-    ${shape.draw()}
-    <text x="10" y="25" fill="${answers.textColor}">${answers.name}</text>
-</svg>
-`;
+    const svgContent = `
+    <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <g>
+            <fill="${answers.shapeColor}" />
+            ${shape.draw()}
+            <text x="150" y="130" font-size="50" text-anchor="middle" fill="${answers.textColor}">${answers.name}</text>
+        </g>
+    </svg>
+    `;
 
-writeToFile('logo.svg', svgContent);
+    writeToFile('logo.svg', svgContent);
 }
 
 function promptUser() {
-inquirer
-    .prompt ([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'Type the three letters that you would like to use for your logo'
-        },
-        {
-            type: 'input',
-            name: 'textColor',
-            message: 'What color do you want to use for your text color in your logo? (enter a color keyword or hex code)'
-        },
-        { 
-            type: 'input',
-            name: 'shapeColor',
-            message: 'What background color do you want to use for the shape in your logo? (enter a color keyword or hex code)'
-        },
-        {
-            type: 'list',
-            name: 'shape',
-            message: 'What shape would you like to use in the background of the logo?',
-            choices: ['Circle', 'Triangle', 'Square']
-        },
-    ])
-    .then((answers) => {
-        if (answers.name.length > 3) {
-            console.log('Please enter only three letters');
-            return;
-        } else {
-            console.log(answers);
-            renderLogo(answers);
-        }
-    })
-    .catch((error) => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Type the three letters that you would like to use for your logo'
+            },
+            {
+                type: 'input',
+                name: 'textColor',
+                message: 'What color do you want to use for your text color in your logo? (enter a color keyword or hex code)'
+            },
+            {
+                type: 'input',
+                name: 'shapeColor',
+                message: 'What background color do you want to use for the shape in your logo? (enter a color keyword or hex code)'
+            },
+            {
+                type: 'list',
+                name: 'shape',
+                message: 'What shape would you like to use in the background of the logo?',
+                choices: ['Circle', 'Triangle', 'Square']
+            },
+        ])
+        .then((answers) => {
+            if (answers.name.length > 3) {
+                console.log('Please enter only three letters');
+                return;
+            } else {
+                console.log(answers);
+                renderLogo(answers);
+            }
+        })
+        .catch((error) => {
             console.log(error);
             console.log('Something went wrong');
-        
-    });
+
+        });
 
 }
 promptUser();
-  
